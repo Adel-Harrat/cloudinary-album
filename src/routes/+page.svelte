cbdb4ef89341706923a7108a3e4d57adadc6c783
Adel-Harrat <adoulamst@gmail.com>
2024-07-17 17:29:47 +0100

    add btton (testing git)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<div class="m-20 text-center">
+	<h1 class="mb-5 text-center text-5xl font-bold tracking-tighter">
+		My gift for radjaa's birthday
+	</h1>
+	<Button>Send this gift to her</Button>
+</div>
